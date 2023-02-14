8375915e81f1cfa4dc8d18c36c208db27d7d4e3a
Marie <mgavillo@student.42.fr>
2023-02-14 13:09:45 +0000

    row test code for AI camera. Need to implement web as it only work on mobile. Test with other models for a better accurancy'

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,28 @@ import { useFonts, Inter_900Black } from "@expo-google-fonts/dev";
 import { ExamplesScreens } from "./screens/ExamplesScreen";
 import { HomeScreen } from "./screens/HomeScreen";
 import { TokenListNavigator } from "./screens/TokenNavigator";
+import TensorCamera from "./screens/TensorCamera";
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <Tab.Navigator
-      initialRouteName="Home"
+      initialRouteName="TensorCamera"
       screenOptions={{
-        tabBarActiveTintColor: "#e91e63",
+        tabBarActiveTintColor: "#FFFFFF",
+        tabBarStyle: {
+          backgroundColor: "rgba(34,36,40,1)",
+          borderTopWidth: 0,
+        },
       }}
     >
       <Tab.Screen
-        name="Home"
-        component={HomeScreen}
+        name="TensorCamera"
+        component={TensorCamera}
         options={{
-          tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="List"
-        component={TokenListNavigator}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Tokens",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bank" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Examples"
-        component={ExamplesScreens}
-        options={{
-          tabBarLabel: "Examples",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
+            <MaterialCommunityIcons name="account" color={color} size={30} />
           ),
         }}
       />
---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,17 @@
+import {
+    View,
+    Button,
+    Image,
+    Pressable,
+    Text,
+    TextInput,
+    TextInputBase,
+    StyleSheet,
+  } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    text: {
+      color:"white"
+    //   width:"max-content"
+    }
+  });
